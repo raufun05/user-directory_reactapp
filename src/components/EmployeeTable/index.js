aa34a06a505991aae264156185e105f55d2e1623
Raufun <raufun05@yahoo.com>
2020-11-08 20:27:24 -0700

    first commit

---
+++ src/components/EmployeeTable/index.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import DataBody from "../UserBody";
+import "./EmployeeTable.css";
+import UserAreaContext from "../../utils/UserAreaContext";
+
+const EmployeeTable = () => {
+    const context = useContext(UserAreaContext);
+    return (
+
+        <div className="datatable mt-5">
+            <table
+                id="table"
+                className="table table-striped table-hover table-condensed"
+            >
+                <thead>
+                    <tr>
+                        {context.developerState.headings.map(({ name, width }) => {
+                            return (
+                                <th
+                                    className="col"
+                                    key={name}
+                                    style={{ width }}
+                                    onClick={() => {
+                                        context.handleSort(name.toLowerCase());
+                                    }}
+                                >
+                                    {name}
+                                    <span className="pointer"></span>
+                                </th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+
+                <DataBody />
+            </table>
+        </div>
+    );
+}
+
+export default EmployeeTable;
---
+++ src/utils/UserAreaContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserAreaContext = React.createContext({
+
+});
+
+export default UserAreaContext;
